081077b33d821c8e0f16633e8e3b55102ea8741e
Kisnan Kankhara <kishankankhara1998@gmail.com>
2020-10-21 00:01:02 +0530

    Output Configured

--- SQlServer.ps1
+++ SQlServer.ps1
@@ -26,96 +26,98 @@ function Get-MachineDetails {
         
     }
     Process {   
-        # Import-Module SQLPS
+        Install-Module SqlServer -AllowClobber
+        
+        Import-Module SQLPS
         Import-Module SqlServer
 
         $server_name = $env:COMPUTERNAME
 
         $WindowsVersion = (systeminfo | Select-String 'OS Version:')[0].ToString().Split(':')[1].Trim()
-        $output += "`nWindows Version:" + $WindowsVersion
+        $output += "`n `nWindows Version:" + $WindowsVersion
         $SqlProductDetails = Invoke-SqlCmd -query "select @@version" -ServerInstance "localhost"
-        $output += "SqlProductDetails: $SqlProductDetails"
+        $output += "`n SqlProductDetails: $SqlProductDetails"
 
         $instanceName = "localhost"
         $server = New-Object -TypeName Microsoft.SqlServer.Management.Smo.Server -ArgumentList $instanceName
         $serverVersion = $server.Information.VersionString
-        $output += "serverVersion: $serverVersion"
+        $output += "`n serverVersion: $serverVersion"
         $productLevel = $server.Information.ProductLevel
-        $output += "productLevel: $productLevel"
+        $output += "`n productLevel: $productLevel"
         $FullTextSearchEnabled = $server.Information.IsFullTextInstalled
-        $output += "FullTextSearchEnabled: $FullTextSearchEnabled"
+        $output += "`n FullTextSearchEnabled: $FullTextSearchEnabled"
         $TotalPhysicalMemory = $server.Information.PhysicalMemory
-        $output += "TotalPhysicalMemory: $TotalPhysicalMemory"
+        $output += "`n TotalPhysicalMemory: $TotalPhysicalMemory"
         $SqlLanguage = $server.Information.Language
-        $output += "SqlLanguage: $SqlLanguage"
+        $output += "`n SqlLanguage: $SqlLanguage"
         $SqlEdition = $server.Information.Edition
-        $output += "SqlEdition: $SqlEdition"
+        $output += "`n SqlEdition: $SqlEdition"
         $dbCollationName = $server.Information.Collation
-        $output += "dbCollationName: $dbCollationName"
+        $output += "`n dbCollationName: $dbCollationName"
         $sql_services = Get-WmiObject -Query "select * from win32_service where PathName like '%%sqlservr.exe%%'" -ComputerName "$server_name" -ErrorAction Stop
 
         $dbName = "PowershellDB"
-        $output += "dbName: $dbName"
+        $output += "`n dbName: $dbName"
         $server = New-Object ('Microsoft.SqlServer.Management.Smo.Server') "LOCALHOST"
 
         $isDatabaseMailEnabled = $server.Configuration.DatabaseMailEnabled.ConfigValue
-        $output += "isDatabaseMailEnabled: isDatabaseMailEnabled"
+        $output += "`n isDatabaseMailEnabled: isDatabaseMailEnabled"
         $databaseMailStatus = $server.Configuration.DatabaseMailEnabled.RunValue
-        $output += "databaseMailStatus: $databaseMailStatus"
+        $output += "`n databaseMailStatus: $databaseMailStatus"
         $FileStreamConfigLevel = $server.Configuration.FilestreamAccessLevel.ConfigValue
-        $output += "FileStreamConfigLevel: $FileStreamConfigLevel"
+        $output += "`n FileStreamConfigLevel: $FileStreamConfigLevel"
         $FileStreamAccessLevel = $server.Configuration.FilestreamAccessLevel.RunValue
-        $output += "FileStreamAccessLevel: $FileStreamAccessLevel"
+        $output += "`n FileStreamAccessLevel: $FileStreamAccessLevel"
 
 
         $isClrEnabled = ( Invoke-Sqlcmd -query "SELECT * FROM sys.configurations WHERE name = 'clr enabled'" ).value  
-        $output += "isClrEnabled: $isClrEnabled"
+        $output += "`n isClrEnabled: $isClrEnabled"
         foreach ($db in $server.Databases) {
             if ($db.Name -eq $dbName) {
                 $dbRecoveryModel = $db.RecoveryModel
-                $output += "dbRecoveryModel: $dbRecoveryModel"
+                $output += "`n dbRecoveryModel: $dbRecoveryModel"
                 $dbCompatibilityLevel = $db.CompatibilityLevel
-                $output += "dbCompatibilityLevel: $dbCompatibilityLevel"
+                $output += "`n dbCompatibilityLevel: $dbCompatibilityLevel"
                 $dbLastBackupDate = $db.LastBackupDate
-                $output += "dbLastBackupDate: $dbLastBackupDate"
+                $output += "`n dbLastBackupDate: $dbLastBackupDate"
             }
         }
 
         $folder = $server.Information.MasterDBLogPath
-        $folder
+        # $folder
         $authenticationMode = $server.Settings.LoginMode
         $output += $authenticationMode
         foreach ($file in Get-ChildItem $folder) {
             if ($file.Name -eq "templog.ldf") {
                 $tempDbLdfPath = $folder + "\" + $file.Name
-                $output += "tempDbLdfPath:$tempDbLdfPath"
+                $output += "`n tempDbLdfPath:$tempDbLdfPath"
             }
             if ($file.Name -eq "templog.mdf") {
                 $tempDbmdfPath = $folder + "\" + $file.Name
-                $output += "tempDbmdfPath: $tempDbmdfPath"
+                $output += "`n tempDbmdfPath: $tempDbmdfPath"
             }
             if ($file.Name -eq $dbName + ".mdf") {
                 $DbMdfFilePath = $folder + "\" + $file.Name
-                $output += "DbMdfFilePath: $DbMdfFilePath"
+                $output += "`n DbMdfFilePath: $DbMdfFilePath"
                 $DbMdfFileSize = ( $file.Length / 1000000 ).ToString() + " MB"
-                $output += "DbMdfFilePath: $DbMdfFileSize"
+                $output += "`n DbMdfFilePath: $DbMdfFileSize"
             }
             if ($file.Name -eq $dbName + "_log.ldf") {
                 $DbLdfFilePath = $folder + "\" + $file.Name
-                $output += "DbLdfFilePath: $DbLdfFilePath"
+                $output += "`n DbLdfFilePath: $DbLdfFilePath"
                 $DbLdfFileSize = ( $file.Length / 1000000 ).ToString() + " MB"
-                $output += "DbLdfFileSize: $DbLdfFileSize"
+                $output += "`n DbLdfFileSize: $DbLdfFileSize"
             }
         }
-        $creds = Get-SqlLogin -ServerInstance "localhost"
-        foreach ($cred in $creds) {
-            if ($cred.LoginType -ne "Certificate") {
-                $cred
-            }
-        }
+        # $creds = Get-SqlLogin -ServerInstance "localhost"
+        # foreach ($cred in $creds) {
+        #     if ($cred.LoginType -ne "Certificate") {
+        #         $cred
+        #     }
+        # }
 
         $backupPath = ($server.Settings.BackupDirectory).ToString() + $dbName + ".bak"
-        $output += "backupPath: $backupPath"
+        $output += "`n backupPath: $backupPath"
 
     }
     End {
--- SSRSConfiguration.ps1
+++ SSRSConfiguration.ps1
@@ -24,55 +24,69 @@ function Get-MachineDetails {
         $output = ""
         $totalspace = 0
         
-        Import-Module SQLPS
+        # Import-Module SQLPS
     }
     Process {   
         
-        $servername = 'NOOBITAXD'
-        $server_name = $env:COMPUTERNAME
+        $servername = $env:COMPUTERNAME
         $instanceName = "localhost"
         $server = New-Object -TypeName Microsoft.SqlServer.Management.Smo.Server -ArgumentList $instanceName
         
         $folder = $server.Information.MasterDBLogPath
-        $folder
+        # $folder
 
         $v = 14
+
         $rs = (Get-WmiObject -namespace root\Microsoft\SqlServer\ReportServer  -class __Namespace).Name
         $nspace = "root\Microsoft\SQLServer\ReportServer\$rs\v$v\Admin"
         $RSServers = Get-WmiObject -Namespace $nspace -class MSReportServer_ConfigurationSetting -ComputerName $servername -ErrorVariable perror -ErrorAction SilentlyContinue
         
         foreach ($r in $RSServers) {
     
-            $ssrsInstanceNamet = $r.InstanceName
+            $ssrsInstanceName = $r.InstanceName
+            $output += "`n ssrsInstanceName: $ssrsInstanceName"
             $ssrsVers = $r.version
+            $output += "`n ssrsVers: $ssrsVers"
             $ssrsDB = $r.DatabaseName
-            $vPath = $r.VirtualDirectoryReportServer
+            # $output += "`n ssrsDB: $ssrsDB"
+            # $vPath = $r.VirtualDirectoryReportServer
+            $output += "`n vPath: $vPath"
             $urls = $r.ListReservedUrls() 
             $urls = $urls.UrlString[0]
             $urls = $urls.Replace('+', $servername) + "/$vPath"
-
+            $output += "`n urls: $urls "
+            
             $ssrsDBmdfPath = $folder + "\" + $ssrsDB +".mdf"
+            $output += "`n ssrsDBmdfPath: $ssrsDBmdfPath"
             $ssrsDBldfPath = $folder + "\" + $ssrsDB +"_log.ldf"
+            $output += "`n ssrsDBldfPath: $ssrsDBldfPath"
             
             $ssrsTempDBldfPath = $folder + "\" + $ssrsDB +"TempDB.mdf"
+            $output += "`n ssrsTempDBldfPath: $ssrsTempDBldfPath"
             $ssrsTempDBldfPath = $folder + "\" + $ssrsDB +"TempDB_log.ldf"
+            $output += "`n ssrsTempDBldfPath: $ssrsTempDBldfPath"
         }
         foreach ($file in Get-ChildItem $folder) {
             if ($file.Name -eq "$ssrsDB.mdf") {
                 $ssrsDBmdfSize  = ($file.Size/1000000).ToString() + " MB"
+                $output += "`n ssrsDBmdfSize: $ssrsDBmdfSize"
             }
             if ($file.Name -eq "$ssrsDB_log.ldf") {
                 $ssrsDBldfSize  = ($file.Size/1000000).ToString() + " MB"
+                $output += "`n ssrsDBldfSize: $ssrsDBldfSize"
             }
             if ($file.Name -eq "$ssrsDBTempDB.mdf") {
                 $ssrsTempDBmdfSize  = ($file.Size/1000000).ToString() + " MB"
+                $output += "`n ssrsTempDBmdfSize: $ssrsTempDBmdfSize"
             }
             if ($file.Name -eq "$ssrsDBTempDB_log.ldf") {
                 $ssrsTempDBldfSize  = ($file.Size/1000000).ToString() + " MB"
+                $output += "`n ssrsTempDBldfSize: $ssrsTempDBldfSize"
             }
         }
     }
     End {
+        return $output | Format-List
     }
 }
 
